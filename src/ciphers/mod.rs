9f48df3a81d994a16750fbffe0418f59f3d80146
SymmetricChaos <ajfraebel@gmail.com>
2021-04-25 11:08:54 -0400

    transposition

--- src/ciphers/mod.rs
+++ src/ciphers/mod.rs
@@ -11,3 +11,8 @@ pub use self::enigma::enigma::{Rotor,Plugboard,Enigma};
 pub use self::enigma::auxiliary::prep_file;
 pub use self::enigma::real_rotors::{ENIGMA_REFLECTORS,ENIGMA_ROTORS};
 
+pub mod polybius;
+pub use self::polybius::polybius::Polybius;
+
+pub mod transposition;
+pub use self::transposition::columnar::Columnar;
---
+++ src/ciphers/transposition/columnar.rs
@@ -0,0 +1,61 @@
+use std::fmt;
+use std::collections::HashMap;
+
+use crate::errors::CipherError;
+
+pub fn pad_with_char(text: &str, length: usize, symbol: char) -> String {
+    let mut text = text.to_string();
+    while text.chars().count() % length != 0 {
+        text.push(symbol)
+    }
+    text
+}
+
+
+// Need a less memory intensive method
+pub struct Columnar {
+    key: Vec<usize>,
+}
+
+impl Columnar {
+    pub fn new(key: Vec<usize>) -> Columnar {
+        Columnar{ key }
+    }
+
+    pub fn encode(&self, text: &str) -> Result<String,CipherError> {
+        let mut columns = Vec::new();
+        for _ in 0..self.key.len() {
+            columns.push(Vec::<char>::new());
+        }
+        let mut symbols = text.chars();
+        for _row in 0..(text.len()/self.key.len()) {
+            for col in columns.iter_mut() {
+                col.push(symbols.next().unwrap_or('X'))
+            }
+        }
+        println!("{:?}",columns);
+
+        let out = "".to_string();
+        Ok(out)
+    }
+
+/*     pub fn decode(&self, text: &str) -> Result<String,CipherError> {
+
+    } */
+}
+
+impl fmt::Display for Columnar {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{:?}",self.key)
+    }
+}
+
+#[test]
+fn columnar() {
+
+    let col = Columnar::new(vec![6,3,2,4,1,5]);
+    println!("Columnar Cipher:{}",col);
+    let plaintext = "WEAREDISCOVEREDFLEEATONCEQKJEU";
+    let ciphertext = col.encode(plaintext).unwrap();
+
+}
