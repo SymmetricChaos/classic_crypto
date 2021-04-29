93242c871cc2bc8ece6225a8fe7109d7ca2cb345
SymmetricChaos <ajfraebel@gmail.com>
2021-04-29 15:47:09 -0400

    updated autokey

--- src/ciphers/vigenere/autokey.rs
+++ src/ciphers/vigenere/autokey.rs
@@ -1,78 +1,46 @@
 use std::fmt;
 use std::collections::VecDeque;
 
-use crate::errors::CipherError;
-use crate::alphabet::ModularAlphabet;
-use crate::modulus::*;
-
-
 
 pub struct Autokey {
-    key: Vec<Modulo>,
+    key: Vec<usize>,
     key_name: String,
-    alpha: ModularAlphabet,
-    whitespace: bool,
+    alphabet: String,
+    length: usize,
 }
 
 impl Autokey {
-    pub fn new(key: &str, alpha: ModularAlphabet) -> Autokey {
+    pub fn new(key: &str, alpha: &str) -> Autokey {
         let key_name = key.to_string();
-        let key = key.chars().map(|x| *alpha.char_to_val(x).unwrap()).collect();
-        Autokey{ key, key_name, alpha, whitespace: false }
+        let key: Vec<usize> = key.chars().map(|x| alpha.chars().position(|c| c == x).unwrap()).collect();
+
+        Autokey{ key, key_name, alphabet: alpha.to_string(), length: alpha.chars().count() }
     }
 
-    pub fn set_whitespace(&mut self, boolean: bool) {
-        self.whitespace = boolean
-    }
-
-    pub fn set_alpha(&mut self, alpha: ModularAlphabet) {
-        self.alpha = alpha
-    }
-
-    pub fn encrypt(&self, text: &str) -> Result<String,CipherError> {
-        let ch = text.to_ascii_uppercase();
-        let mut out = Vec::new();
-        let mut akey: VecDeque<Modulo> = self.key.clone().into_iter().collect();
-        for c in ch.chars() {
-            if c.is_ascii_whitespace() {
-                if self.whitespace {
-                    out.push(c);
-                }
-            } else {
-                let v = match self.alpha.char_to_val(c) {
-                    Some(m) => *m,
-                    None => continue
-                };
-                akey.push_back(v);
-                let x = v + akey.pop_front().unwrap();
-                out.push(*self.alpha.val_to_char(x).unwrap())
-            }
+    pub fn encrypt(&self, text: &str) -> String {
+        let mut out = "".to_string();
+        let mut akey: VecDeque<usize> = self.key.clone().into_iter().collect();
+        let nums: Vec<usize> = text.chars().map( |x| self.alphabet.chars().position(|c| c == x).unwrap() ).collect();
+        for n in nums {
+            akey.push_back(n);
+            let k = akey.pop_front().unwrap();
+            out.push(self.alphabet.chars().nth( (n+k)%self.length ).unwrap() )
         }
-        let val: String = out.iter().collect();
-        Ok(val)
+        out
     }
 
-    pub fn decrypt(&self, text: &str) -> Result<String,CipherError> {
-        let ch = text.to_ascii_uppercase();
-        let mut out = Vec::new();
-        let mut akey: VecDeque<Modulo> = self.key.clone().into_iter().collect();
-        for c in ch.chars() {
-            if c.is_ascii_whitespace() {
-                if self.whitespace {
-                    out.push(c);
-                }
-            } else {
-                let v = match self.alpha.char_to_val(c) {
-                    Some(m) => *m,
-                    None => continue
-                };
-                let x = v - akey.pop_front().unwrap();
-                akey.push_back(x);
-                out.push(*self.alpha.val_to_char(x).unwrap())
-            }
+    pub fn decrypt(&self, text: &str) -> String {
+        let mut out = "".to_string();
+        let mut akey: VecDeque<usize> = self.key.clone().into_iter().collect();
+        let nums: Vec<usize> = text.chars().map( |x| self.alphabet.chars().position(|c| c == x).unwrap() ).collect();
+        for n in nums {
+            let k = akey.pop_front().unwrap();
+            let v = (n + self.length - k ) % self.length;
+            let c = self.alphabet.chars().nth( v ).unwrap();
+            out.push(c);
+            akey.push_back(v);
         }
-        let val: String = out.iter().collect();
-        Ok(val)
+        out
     }
 }
 
@@ -83,13 +51,12 @@ impl fmt::Display for Autokey {
 }
 
 #[test]
-fn affine() {
-    let alpha = ModularAlphabet::new("ABCDEFGHIJKLMNOPQRSTUVWXYZ");
-    let mut aff = Autokey::new("SECRET", alpha);
-    aff.set_whitespace(true);
-    let plaintext = "the quick brown fox jumps over the lazy dog";
-    let ciphertext = aff.encrypt(plaintext).unwrap();
-    let cleartext = aff.decrypt(&ciphertext).unwrap();
+fn autokey() {
+    use crate::auxiliary::LATIN26;
+let auto = Autokey::new("SECRET", LATIN26);
+    let plaintext = "THEQUICKBROWNFOXJUMPSOVERTHELAZYDOG";
+    let ciphertext = auto.encrypt(plaintext);
+    let cleartext = auto.decrypt(&ciphertext);
 
     println!("{}\n{}\n{}",plaintext,ciphertext,cleartext);
     
