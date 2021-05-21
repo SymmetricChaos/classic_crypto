ede5c807784c40ed828969dc994d8a171b781ade
SymmetricChaos <ajfraebel@gmail.com>
2021-05-21 08:22:38 -0400

    godel

--- src/codes/godel/godel.rs
+++ src/codes/godel/godel.rs
@@ -1,5 +1,5 @@
 use crate::auxiliary::PrimeSieve;
-
+use num::{BigUint, Integer, Num, One};
 
 pub struct Godel<'a> {
     alphabet: &'a str,
@@ -8,7 +8,6 @@ pub struct Godel<'a> {
 impl Godel<'_> {
 
     pub fn new<'a>(alphabet: &'a str) -> Godel {
-
         Godel{ alphabet }
     }
 
@@ -18,13 +17,16 @@ impl Godel<'_> {
 impl crate::Code for Godel<'_> {
 
     fn encode(&self, text: &str) -> String {
-        let mut out = 1usize;
+        // We need big integers to encode anything even slightly useful
+        let mut out = BigUint::one();
         let mut pset = PrimeSieve::new();
         let alpha_vec: Vec<char> = self.alphabet.chars().collect();
 
         for t in text.chars() {
             let n = alpha_vec.iter().position(|x| *x == t).unwrap() + 1;
-            out = out * pset.next().unwrap().pow(n as u32)
+            let p = pset.next().unwrap();
+            let t = p.pow(n as u32);
+            out *= BigUint::from(t);
             
         }
 
@@ -32,14 +34,30 @@ impl crate::Code for Godel<'_> {
     }
 
     fn decode(&self, text: &str) -> String {
+        let mut num = BigUint::from_str_radix(text,10).unwrap();
+        let mut pset = PrimeSieve::new();
+        let alpha_vec: Vec<char> = self.alphabet.chars().collect();
         let mut out = String::new();
+        while !num.is_one() {
+            let p = BigUint::from(pset.next().unwrap());
+            let mut ctr = 0;
+            while num.is_multiple_of(&p) {
+                ctr += 1;
+                num = num.div_floor(&p)
+            }
+            out.push(alpha_vec[ctr-1])
+
+        }
 
         out
     }
 
     fn char_map(&self) -> String {
         let mut out = String::new();
-
+        let length = self.alphabet.chars().count();
+        for (l,n) in self.alphabet.chars().zip(1..=length) {
+            out.push_str(&format!("{}  ^{}\n",l,n))
+        }
         out
     }
 }
--- src/codes/godel/godel_tests.rs
+++ src/codes/godel/godel_tests.rs
@@ -7,14 +7,14 @@ mod vigenere_tests {
 
     #[test]
     fn godel_test() {
-        let ascii = Godel::new(LATIN26);
-        let coded = ascii.encode("ATTACK");
-        let decoded = ascii.decode(&coded);
+        let godel = Godel::new(LATIN26);
+        let coded = godel.encode("ATTACK");
+        let decoded = godel.decode(&coded);
 
-        println!("{}",coded);
+        println!("{}",godel.char_map());
 
-        //assert_eq!(coded,"");
-        //assert_eq!(decoded,PLAINTEXT);
+        assert_eq!(coded,"11104733683275566574453416633605957031250");
+        assert_eq!(decoded,"ATTACK");
     }
 
 
